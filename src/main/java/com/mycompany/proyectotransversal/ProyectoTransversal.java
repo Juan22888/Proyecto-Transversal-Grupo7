4a0e15e56ba9502f75ca24c86160749cabbe76cb
Barzola juan <barzolajuan661@gmail.com>
2025-09-29 19:23:46 -0300

    Creacion del proyecto y packages

---
+++ src/main/java/com/mycompany/proyectotransversal/ProyectoTransversal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectotransversal;
+
+/**
+ *
+ * @author LaMaquina
+ */
+public class ProyectoTransversal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
